2b94d7b84095aeccde54a4d3f9a9824bf7ab4177
Kai Yamada <gpioblink@aol.com>
2020-11-13 14:36:05 +0900

    fix recv

--- ex06/one2one.c
+++ ex06/one2one.c
@@ -5,18 +5,22 @@ int main(int argc, char *argv[] )
   int np, id;
   MPI_Init(&argc, &argv);
 
+  // 起動されたプロセス数の取得
   MPI_Comm_size(MPI_COMM_WORLD, &np);
+  // 自分のidを取得
   MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-  int data = 3;
-  MPI_Send(&data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-
-  MPI_Status status:
   int data;
-  MPI_Recv(&data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
-  printf("receive %d from %d\n", data, 1);
+  MPI_Status status;
 
-  printf("Hello world from process %d (%d)\n", id, np);
+  if(id != 0) {
+    data = 3;
+    MPI_Send(&data, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+  } else {
+    MPI_Recv(&data, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+    printf("receive %d from %d\n", data, id);
+  }
+
   MPI_Finalize();
   return 0;
 }
