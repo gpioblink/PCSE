4da01e2e1d446d2403c91fdd36a5023d18c72d2a
s1260250 <s1260250@u-aizu.ac.jp>
2020-11-24 16:29:51 +0900

    fix calc

--- ex07/pi.c
+++ ex07/pi.c
@@ -23,36 +23,41 @@ int main(int argc,char *argv[]) {
 
   MPI_Status status;
 
+  // printf("%d is active\n", id);
   if(id == 0) {
     h = 1.0/(double)n;
-    for(i=1; i<np; i++) {
+    for(i=0; i<np; i++) {
       idxs = (n/np)*i;
-      idxe = (n/np)*(i+1) + (i-1 == np ? n % np : 0);
+      idxe = (n/np)*(i+1);
       MPI_Send(&h, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
       MPI_Send(&idxs, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
       MPI_Send(&idxe, 1, MPI_INT, i, 2, MPI_COMM_WORLD);  
     }
+  }
+
+  sum = 0.0;
+  MPI_Recv(&h, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+  MPI_Recv(&idxs, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+  MPI_Recv(&idxe, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+  startwtime = MPI_Wtime();
+  for (i = idxs; i < idxe; i++) {
+    x = h*((double)i - 0.5);
+    sum += f(x);
+  }
+  endwtime = MPI_Wtime();
+  printf("process %d finished for %lf sec\n", id, endwtime - startwtime);
+  MPI_Send(&sum, 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
+
+  if(id == 0) {
     realsum = 0.0;
-    for(i=1; i<np; i++) {
+    for(i=0; i<np; i++) {
       MPI_Recv(&sum, 1, MPI_DOUBLE, i, 3, MPI_COMM_WORLD, &status);
       realsum += sum;
     }
     mypi = h*realsum;
-    printf("pi is approximately %.16f, Error is %.16f\n", mypi, fabs(mypi - PI25DT)/PI25DT);
-  } else {
-    sum = 0.0;
-    MPI_Recv(&h, 1, MPI_DOUBLE, id, 0, MPI_COMM_WORLD, &status);
-    MPI_Recv(&idxs, 1, MPI_INT, id, 1, MPI_COMM_WORLD, &status);
-    MPI_Recv(&idxe, 1, MPI_INT, id, 2, MPI_COMM_WORLD, &status);
-    startwtime = MPI_Wtime();
-    for (i = idxs; i < idxe; i++) {
-      x = h*((double)i - 0.5);
-      sum += f(x);
-    }
-    endwtime = MPI_Wtime();
-    MPI_Send(&sum, 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
-    printf("process %d finished for %ld sec\n", id, endwtime - startwtime);
+    printf("pi is approximately %.16f, Error is %.16f\n", mypi, fabs(mypi - PI25DT));
   }
+
   MPI_Finalize();
   return 0;
 }
